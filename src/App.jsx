c74fa62874e26f42448b00a948a511bccc80a157
Sheba <bathsheba.nyairo@student.moringaschool.com>
2026-02-13 15:12:21 +0300

    updated productCard, productList, app.css, app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
 import React, { useState } from 'react';
 import ProductList from './components/ProductList';
+import data from '../db.json';
+import { Button, Stack } from '@mui/material';
 
 const App = () => {
   // TODO: Define initial product data
+  const [products, setProducts] = useState(data.products);
 
   // TODO: Implement state to manage filtering
+    // filter state
+  const [filter, setFilter] = useState("all");
+
+  const handleRemove = (id) => {
+  setProducts(products.filter(product => product.id !== id));
+};
+
 
   // TODO: Implement logic to filter products based on availability
+    const filteredProducts = products.filter(product => {
+    if (filter === "inStock") return product.inStock === true;
+    if (filter === "outOfStock") return product.inStock === false;
+    return true; // show all
+  });
 
   return (
     <div>
-      <h1>{/* TODO: Add 'Product Dashboard' title here */}</h1>
+      <h1>Product Dashboard</h1>
       
       {/* TODO: Add buttons to allow filtering by availability */}
+        <Stack direction="row" spacing={2} justifyContent="center">
+        <Button variant="contained" onClick={() => setFilter("all")}>
+          All
+        </Button>
+        <Button variant="contained" color="success" onClick={() => setFilter("inStock")}>
+          In Stock
+        </Button>
+        <Button variant="contained" color="error" onClick={() => setFilter("outOfStock")}>
+          Out of Stock
+        </Button>
+      </Stack>
 
       {/* TODO: Render the ProductList component and pass filtered products */}
-      
+      <ProductList products={filteredProducts}  onRemove={handleRemove}/>
     </div>
   );
 };
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ProductCard from './ProductCard';
 
-const ProductList = ({ products }) => {
+const ProductList = ({ products, onRemove }) => {
   // TODO: Check if the product list is empty and display a message if needed
+
+
+  if (!products || products.length === 0) {
+    return <p>No products found.</p>;
+  }
 
   return (
     <div>
       {/* TODO: Iterate over the products array and render a ProductCard for each product */}
+
+       {products.map(product => (
+        <ProductCard  key={product.id}
+     product={product}
+     onRemove={onRemove} />
+      ))}
+
     </div>
   );
 };
